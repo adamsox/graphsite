7775c868ddc45a3ec7b0b6f0e222e89482ab26c6
Xavier Ifill <xifill@uoguelph.ca>
2022-01-23 16:47:36 -0500

    Update scraper.js- naming conventions and more readability

--- scraper.js
+++ scraper.js
@@ -10,8 +10,9 @@ import playwright from 'playwright'
 import fs from 'fs'
 
 (async () => {
+  
   // Array of JSON objects storing information for each course
-  let allData = []
+  let coursesInfo = []
 
   // Accessing all programs offered at Guelph 
   // with their respective links in order to access all courses 
@@ -24,17 +25,17 @@ import fs from 'fs'
   const programUrls = await page.$eval(".az_sitemap", (all_items) => {
     
     // Array of links to courses within programs
-    const data = [];
+    const courseLinks = [];
 
     const links = all_items.getElementsByTagName('a');    
     
     // Pushing all links that were scraped from the 'az_sitemap' HTML DOM 
     for (let c = 0; c < links.length; c++){
-        data.push(links[c].href);
+        courseLinks.push(links[c].href);
     }
 
     // Returning array of links
-    return data;
+    return courseLinks;
   });
 
   // Iterating through links to each course's info
@@ -46,11 +47,11 @@ import fs from 'fs'
         tmp.forEach( courseObj => {
           
           // Pushing course info object to array of all courses
-          allData.push(courseObj)
+          coursesInfo.push(courseObj)
         });
         
         // Writing all data to JSON file
-        fs.writeFile('courses.json', JSON.stringify(allData), err => {if (err) throw err});
+        fs.writeFile('courses.json', JSON.stringify(coursesInfo), err => {if (err) throw err});
       }
   });
 
@@ -64,31 +65,37 @@ import fs from 'fs'
  * @param {*} url // url with program-specific course information 
  */
 async function scrapFunc(url) {
+    
     // Launching browser and creating page
     const browser = await playwright.chromium.launch();
     const page = await browser.newPage();
+    
     // Accessing program-specific web page 
     await page.goto(url);
 
     // Accessing all 'courseblock' classes 
     // 'courseblock' classes hold all information regarding each course (relative to program)
     const courses = await page.$$eval(".courseblock", (all_items) => {
-      const data = [];
+      const courseInfoObjs = [];
   
       all_items.forEach((course) => {
+        
         // Getting course code 
         const cc = course.querySelector(".detail-code").innerText;
+        
         // Getting course title/description 
         const desc =  course.querySelector('.detail-title').innerText
+        
         // Getting credit worth
         const cred = course.querySelector(".detail-hours_html").innerText;
+        
         // Getting semesters that the course is offered in  
         const off = course.querySelector('.detail-typically_offered')?.innerText;
 
         // Pushing extracted data to array of JSON objects 
-        data.push({ cc, cred, desc, off });
+        courseInfoObjs.push({ cc, cred, desc, off });
       });
-      return data;
+      return courseInfoObjs;
     });
 
     
