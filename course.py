51fa2bdae2961692608773f2f89af9264c1c5105
Jong Bum Kim <jongbum@uoguelph.ca>
2022-01-23 01:22:34 -0500

    Delete course.py

--- course.py
+++
@@ -1,6 +0,0 @@
-class course:
-  def __init__(self, course_code, credit_count, season, desc):
-    self.course_code = course_code
-    self.credit_count = credit_count
-    self.season = season
-    self.desc = desc
