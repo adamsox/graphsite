1f1dbd959eb74e17e7aaacfdac1bc4983a3f5d01
Richard Nguyen <rnguye03@uoguelph.ca>
2022-03-28 07:38:45 -0400

    Update setup file with deployment testing

---
+++ setup.sh
@@ -0,0 +1,74 @@
+sudo apt-get upgrade
+sudo apt-get update
+
+sudo apt install libssl1.0-dev
+sudo apt install nodejs-dev
+sudo apt install node-gyp
+sudo apt install npm
+
+# Python Deployment Testing
+pip3 install --upgrade pip
+pip3 install playwright
+playwright install
+
+pip3 install asyncio
+
+
+#Python:
+sudo apt install python3-venv
+sudo -m venv myprojectenv
+
+echo 'wheel and uwsgi'
+pip3 install wheel
+pip3 install uwsgi
+
+echo 'python3-pip'
+sudo apt install python3-pip
+
+echo 'npm'
+sudo apt install npm
+
+# Setup:
+# for http testing
+# sudo ufw allow 5000
+
+# Commands Dev:
+# python3 myproject.py
+
+#(not sure how to run a command in a child directory from a script)
+# ( cd myprojectenv ; uwsgi –socket 0.0.0.0:5000 –protocol=http -w wsgi:app )
+
+mkdir ~/etc/systemd
+mkdir ~/etc/systemd/system/
+mv myproject.service ~/etc/systemd/system/
+
+# start
+# sudo systemctl start myproject
+# enable at boot
+# sudo systemctl enable myproject
+# status
+# sudo systemctl status myproject
+
+mkdir ~/etc/nginx/
+mkdir ~/etc/nginx/sites-available/
+
+mv myproject ~/etc/nginx/sites-available/
+
+sudo ln -s /etc/nginx/sites-available/myproject /etc/nginx/sites-enabled
+
+# error check
+# sudo nginx -t
+
+# sudo systemctl restart nginx
+
+sudo ufw allow ‘Nginx Full’
+
+# for http Flask testing
+# sudo ufw delete allow 5000
+
+sudo ufw delete allow ‘Nginx HTTP’
+
+# for certificates when domain is setup
+# sudo add-apt-repository ppa:certbot/certbot
+# sudo apt install python3-certbot-nginx
+# sudo certbot –nginx -d <domain_name> -d www.<domain_name>
